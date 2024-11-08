d13e911312ec72cf06f2bb592f72a44e4151d8fb
Cynthia <140533208+CynthiaC98389@users.noreply.github.com>
2024-11-08 03:48:37 -0600

    Add files via upload

---
+++ Formulario IBL/index.js
@@ -0,0 +1,12 @@
+let monthName=["Enero","Febrero","Marzo", "Abril", "Mayo","Junio", "Julio","Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+let now = new Date ();
+let day = now.getDate();
+let month = now.getMonth ();
+let currentMonth = month;
+let year= now.getFullYear ();
+
+function initCalendar () {
+    
+}
