ffae2bb9dc1e1f62bb91dcc413762e0d5d88f77c
CynthiaC98389 <cynthia.cast3llanos@gmail.com>
2024-11-19 22:03:12 -0600

    nuevo form de registro de productos

--- Formulario.js
+++ Formulario.js
@@ -1,62 +1,86 @@
 document.getElementById("formulario-cotizacion").addEventListener("submit", function(event) {
     event.preventDefault(); // Evita el envío inmediato del formulario
 
-    // Obtener valores de los campos
     const nombre = document.getElementById("nombre").value.trim();
-    const correo = document.getElementById("correo").value.trim();
-    const telefono = document.getElementById("telefono").value.trim();
-    const servicio = document.getElementById("seleccion-servicio").value;
-    const mensaje = document.getElementById("mensaje").value.trim();
-    const fechaCotizacion = document.getElementById("fecha-cotizacion").value;
+    const descripcion = document.getElementById("descripcion").value.trim();
+    const marca = document.getElementById("marca").value.trim();
+    const categoria = document.getElementById("seleccion-categoria").value;
+    const precio = document.getElementById("precio").value.trim();
+    const cantidad = document.getElementById("cantidad").value.trim();
+    const codigo = document.getElementById("codigo").value.trim();
+    const imagen= document.getElementById("imagen").value.trim();
+
+    // Verificar si el campo "categoría" está vacío
+    if (!categoria) {
+        alert("Por favor, selecciona una categoria.");
+        return; // Detiene el envío si falta la selección de la categoría
+    }
 
     // Verificar si el campo "nombre" está vacío
     if (!nombre) {
-        alert("Por favor, ingresa tu nombre.");
+        alert("Por favor, ingresa el nombre del producto.");
         return; // Detiene el envío si falta el nombre
     }
 
-    // Verificar si el campo "correo electrónico" está vacío
-    if (!correo) {
-        alert("Por favor, ingresa tu correo electrónico.");
-        return; // Detiene el envío si falta el correo
+    // Verificar si el campo "descripcion" está vacío
+    if (!descripcion) {
+        alert("Por favor, ingresa una descripción.");
+        return; // Detiene el envío si falta la descripcion
     }
 
-    // Verificar si el campo "teléfono" está vacío
-    if (!telefono) {
-        alert("Por favor, ingresa tu número de teléfono.");
-        return; // Detiene el envío si falta el teléfono
+    // Verificar si el campo "marca" está vacío
+    if (!marca) {
+        alert("Por favor, ingresa una marca.");
+        return; // Detiene el envío si falta la marca
     }
     
-    // Verificar si el campo "teléfono" tiene solo números
-    if (isNaN(telefono)) {
-    alert("Por favor, ingresa solo números en el campo de teléfono.");
+    // Verificar si el campo "codigo" está vacío
+    if (!codigo) {
+        alert("Por favor, ingresa el código del producto.");
+        return; // Detiene el envío si falta el codigo
+    }
+    
+    // Verificar si el campo "codigo" tiene solo números
+    if (isNaN(codigo)) {
+    alert("Por favor, ingresa solo números en el campo de código.");
     return; // Detiene el envío si hay letras u otros caracteres
     }
 
 
-    // Verificar si el campo "servicio" está vacío
-    if (!servicio) {
-        alert("Por favor, selecciona un servicio.");
-        return; // Detiene el envío si falta la selección del servicio
+    // Verificar si el campo "precio" está vacío
+    if (!precio) {
+        alert("Por favor, ingresa un precio.");
+        return; // Detiene el envío si falta el precio
+    }
+    if (isNaN(precio)) {
+        alert("Por favor, ingresa solo números en el campo de precio.");
+        return; // Detiene el envío si hay letras u otros caracteres
     }
 
-    // Verificar si el campo "mensaje" está vacío
-    if (!mensaje) {
-        alert("Por favor, ingresa un mensaje.");
-        return; // Detiene el envío si falta el mensaje
+    
+    // Verificar si el campo "cantidad" está vacío
+    if (!cantidad) {
+        alert("Por favor, ingresa una cantidad.");
+        return; // Detiene el envío si falta la cantidad
+    }
+    if (isNaN(cantidad)) {
+        alert("Por favor, ingresa solo números en el campo de cantidad.");
+        return; // Detiene el envío si hay letras u otros caracteres
     }
 
-    // Verificar si el campo "fecha" está vacío
-    if (!fechaCotizacion) {
-        alert("Por favor, selecciona una fecha.");
-        return; // Detiene el envío si falta la fecha
+    // Verificar si el campo "imagen" está vacío
+    if (!imagen) {
+        alert("Por favor, ingresa una imagen.");
+        return; // Detiene el envío si falta una imagen
     }
+    
+     // En caso que todos los datos sean llenados correctamente
+     alert("Formulario enviado con éxito.");
 
-    // Si todos los campos están completos, mostrar la fecha seleccionada
-    alert("Fecha seleccionada: " + fechaCotizacion);
+
 
     // Recargar la página después de un pequeño retraso
     setTimeout(() => {
         location.reload();
     }, 500); // Recarga la página después de 500 ms (medio segundo)
-});
+    });
