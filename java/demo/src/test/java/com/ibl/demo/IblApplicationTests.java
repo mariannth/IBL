304e3351a85e5e84922110f7945cc36994c5ca13
Mariana Torres <marith.3289@gmail.com>
2024-12-07 20:05:51 -0600

    Agregando carpetas a la estructura

---
+++ java/demo/src/test/java/com/ibl/demo/IblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
